8dbb975216a27f1819d42f07628c08bf0fbfcc7a
samupd2013 <36841396+samupd2013@users.noreply.github.com>
2020-08-12 01:16:34 +0200

    Add files via upload

--- Model/account.cpp
+++ Model/account.cpp
@@ -2,7 +2,11 @@
 
 Account::Account(){}
 
-Account::Account(QString us, QString pw, acc t) : username(us), password(pw), type(t) {
+Account::Account(string us, string pw, acc t) : username(us), password(pw), type(t) {}
 
+vector<Song*>::iterator Account::searchSong(const Song& s){
+    vector<Song*>::iterator it = songs.begin();
+    while (it != songs.end() && **it != s)
+        it++;
+    return it;
 }
-
